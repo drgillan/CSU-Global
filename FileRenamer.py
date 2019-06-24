dfedee137d1b2fd1e66d687806d6ed8eaef1b72b
drgillan <52140012+drgillan@users.noreply.github.com>
2019-06-23 18:37:15 -0600

    Update FileRenamer.py

--- FileRenamer.py
+++ FileRenamer.py
@@ -1,8 +1,8 @@
 import os
-os.chdir('.')
+os.chdir('YOURPATH')
 i=1
 for file in os.listdir():
   src=file
-  dst="2019_"+str(i)+".docx"
+  dst="YOURSTRING"+str(i)+".YOURFILEEXTENSION"
   os.rename(src,dst)
   i+=1
